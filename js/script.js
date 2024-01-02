27377c83ebe9f8116a211415e12fb7c449fb6ff2
Sofia Weng <sowe0001@stud.kea.dk>
2024-01-02 19:42:44 +0100

    hover på herobutton med javascript

--- js/script.js
+++ js/script.js
@@ -12,3 +12,24 @@ document.querySelectorAll(".nav-link").forEach((n) =>
     navmenu.classList.remove("active");
   })
 );
+
+const pil = document.querySelector(".pil");
+const button = document.querySelector(".button");
+const buttontext = document.querySelector(".buttontext");
+window.addEventListener("load", sidenVises);
+function sidenVises() {
+  console.log("sidenvises");
+  //tilføjer eventlistener på hero-button
+  pil.addEventListener("mouseover", herobutton_mouseover);
+  pil.addEventListener("mouseout", herobutton_mouseout);
+  button.addEventListener("mouseover", herobutton_mouseover);
+  button.addEventListener("mouseout", herobutton_mouseout);
+}
+function herobutton_mouseover() {
+  button.classList.add("buttonhover");
+  buttontext.classList.add("hoveronp");
+}
+function herobutton_mouseout() {
+  button.classList.remove("buttonhover");
+  buttontext.classList.remove("hoveronp");
+}
