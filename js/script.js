c5023c7e441e5975b319c66ded1c5660f69ccd37
Sofia Weng <sowe0001@stud.kea.dk>
2024-01-09 22:42:49 +0100

    sletter nogle items

--- js/script.js
+++ js/script.js
@@ -17,10 +17,6 @@ const pil = document.querySelector(".pil");
 const button = document.querySelector(".button");
 const buttontext = document.querySelector(".buttontext");
 const herotext = document.querySelector(".hero-text");
-/*const tema02 = document.querySelectorAll(".grid_1fr-2fr");
-const tema03 = document.querySelector("#tema03");
-const tema04 = document.querySelector("#tema04");
-const tema05 = document.querySelector("#tema05");*/
 
 window.addEventListener("load", sidenVises);
 function sidenVises() {
@@ -28,7 +24,4 @@ function sidenVises() {
   //tilføjer animation på tekst på forsiden
   herotext.classList.add("flyvop");
   button.classList.add("flyvop", "delay2");
-
-  /*tilføjer animation på temaerne på portfolio
-   tema02.classList.add("flyvop");*/
 }
